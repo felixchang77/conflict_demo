9620c5a896f3643009f2257d4faab77201b34a02
Felix Chang <felixchang77@gmail.com>
2022-01-07 21:27:51 -0500

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,17 +1,9 @@
 def hello_world():
-<<<<<<< HEAD
-    print(f'Hello World {state}!')
+    print(f'Hello World {city},{state}!')
 
 
 def main():
-    hello_world('New York')
-=======
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> c9fc2cf3bd349d402bb417362a2f3d0924e0b42d
+    hello_world('NYC', 'New York')
 
 
 main()
